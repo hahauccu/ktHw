9569f6bdb7221406c0967d9cd0ddd81323a7500b
victor <zxc25852ad40@gmail.com>
2019-03-01 18:25:33 +0800

    加上了重複訂票及累計功能

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -2,22 +2,37 @@ package com.train;
 
 import java.util.Scanner;
 
-public class Tester {
+public class
+Tester {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
-        int totalTicket ;
-        int roundTicket ;
-        do
+        int totalTicket =0;
+        int roundTicket =0;
+        int totalTicketInput=0;
+        int roundTicketInput=0;
+        while(totalTicket < roundTicket || roundTicketInput != -1 && totalTicketInput!= -1)
         {
             System.out.println("Please enter number of tickets: ");
-            totalTicket =scanner.nextInt();
+            totalTicketInput = scanner.nextInt();
+
             System.out.println("How many round-trip tickets: ");
-            roundTicket =scanner.nextInt();
-            if(totalTicket < roundTicket)
+            roundTicketInput = scanner.nextInt();
+
+            if(roundTicketInput != -1 && totalTicketInput!= -1)
             {
-                System.out.println("total ticket can not be less than round tiecket");
+                if(totalTicketInput < roundTicketInput)
+                {
+                    System.out.println("total ticket can not be less than round tiecket");
+                }
+                else
+                {
+                    roundTicket +=roundTicketInput;
+                    totalTicket +=totalTicketInput;
+
+                }
             }
-        }while(totalTicket < roundTicket);
+        }
+
 
         Ticket ticket = new Ticket(totalTicket,roundTicket);
         ticket.conculate();
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -4,19 +4,34 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`);
-    var totalTicket:Int;
-    var roundTicket:Int;
-    do
+    var totalTicket=0;
+    var roundTicket=0;
+    var roundTicketInput=0;
+    var totalTicketInput=0;
+
+
+    while(totalTicket < roundTicket || roundTicketInput != -1 && totalTicketInput!= -1)
     {
         println("Please enter number of tickets: ");
-        totalTicket =scanner.nextInt();
+        totalTicketInput = scanner.nextInt();
+
         println("How many round-trip tickets: ");
-        roundTicket =scanner.nextInt();
-        if(totalTicket < roundTicket)
+        roundTicketInput = scanner.nextInt();
+
+        if(roundTicketInput != -1 && totalTicketInput!= -1)
         {
-            println("total ticket can not be less than round tiecket");
+            if(totalTicketInput < roundTicketInput)
+            {
+                println("total ticket can not be less than round tiecket");
+            }
+            else
+            {
+                roundTicket +=roundTicketInput;
+                totalTicket +=totalTicketInput;
+
+            }
         }
-    }while(totalTicket < roundTicket);
+    }
     val ticket = TicketKt(totalTicket,roundTicket);
     ticket.conculate();
 
@@ -30,6 +45,6 @@ class TicketKt(var totalTicket: Int , var roundTicket: Int )
         var total=0;
         total += (totalTicket - roundTicket) * 1000;
         total += roundTicket * 1800;
-        print("Totle tickets : " + totalTicket + "\nRound tip : "+ roundTicket + "\nTotal : " + total );
+        print("Totle tickets : $totalTicket\nRound tip : $roundTicket\nTotal : $total" );
     }
 }
